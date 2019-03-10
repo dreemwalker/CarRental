447648fc1a947dc8babc5ea77aee690ed98a8cb9
Dalatar <dalatarion@gmail.com>
2019-03-10 12:29:58 +0300

    add client structure and routing

--- Client/ClientApp/src/app/app-routing.module.ts
+++ Client/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {AboutComponent} from './about/about.component';
+import {ErrorComponent} from './error/error.component';
+import {UsersComponent} from './users/users.component';
+import {CarsComponent} from './cars/cars.component';
+import {OrdersComponent} from './orders/orders.component';
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'cars', component: CarsComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Client/ClientApp/src/app/app.module.ts
+++ Client/ClientApp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CarsComponent } from './cars/cars.component';
+import { UsersComponent } from './users/users.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    CarsComponent,
+    UsersComponent,
+    OrdersComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
